0b4d40af137052e345cb491adbe3e69964ec33ea
Erik Carlsson <info@code301.com>
2014-12-09 22:49:13 +0100

    "Init."

---
+++ lib/Syntax/Feature/Qs.pm
@@ -0,0 +1,28 @@
+package Syntax::Feature::Qs;
+
+use strict;
+use warnings;
+use 5.10.1;
+
+
+1;
+
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Syntax::Feature::Qs - Short intro
+
+=head1 SYNOPSIS
+
+  use Syntax::Feature::Qs;
+
+=head1 DESCRIPTION
+
+Syntax::Feature::Qs is ...
+
+=head1 SEE ALSO
+
+=cut
