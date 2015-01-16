a78a53f507bbb0c4e33cfa29173797075832ebe8
Erik Carlsson <info@code301.com>
2015-01-16 17:22:07 +0100

    Cleaned up doc (now uses Pod::Weaver), removed Syntax::Feature::Simple::V2 and aliased dependency.

--- lib/Syntax/Feature/Qs.pm
+++ lib/Syntax/Feature/Qs.pm
@@ -1,20 +1,25 @@
 use strict;
+use warnings;
 
 package Syntax::Feature::Qs;
+
+# VERSION
+# ABSTRACT: Trim leading whitespace from all lines in a string
 
 use Devel::Declare 0.006007 ();
 use B::Hooks::EndOfScope 0.09;
 use Sub::Install 0.925 qw/install_sub/;
 
-use aliased 'Devel::Declare::Context::Simple', 'Context';
+use Devel::Declare::Context::Simple;
 
-use syntax qw|simple/v2|;
 use namespace::clean;
 
 my %quote_op = qw(qs q qqs qq);
 my @new_ops = keys %quote_op;
 
-method install($class: %args) {
+sub install {
+    my $class = shift;
+    my %args = @_;
     my $target = $args{'into'};
 
     Devel::Declare->setup_for($target => {
@@ -22,7 +27,7 @@ method install($class: %args) {
             my $name = $_;
             ($name => {
                 const => sub {
-                    my $context = Context->new;
+                    my $context = Devel::Declare::Context::Simple->new;
                     $context->init(@_);
                     return $class->_transform($name, $context);
                 },
@@ -42,7 +47,7 @@ method install($class: %args) {
     return 1;
 }
 
-method _run_callback {
+sub _run_callback {
     return sub ($) {
         my $string = shift;
         $string =~ s{^\h+|\h+$}{}gms;
@@ -50,7 +55,11 @@ method _run_callback {
     };
 }
 
-method _transform ($class: $name, $ctx) {
+sub _transform {
+    my $class = shift;
+    my $name = shift;
+    my $ctx = shift;
+
     $ctx->skip_declarator;
     my $length = Devel::Declare::toke_scan_str($ctx->offset);
     my $string = Devel::Declare::get_lex_stuff;
@@ -76,10 +85,6 @@ method _transform ($class: $name, $ctx) {
 __END__
 
 =encoding utf-8
-
-=head1 NAME
-
-Syntax::Feature::Qs - Trim whitespace from all lines
 
 =head1 SYNOPSIS
 
@@ -108,27 +113,10 @@ Their purpose is to automatically trim leading and trailing horizontal whitespac
 
 =head1 SEE ALSO
 
-=over 4
-
-=item L<Syntax::Feature::Ql> (which served as a base for this)
-
-=item L<Syntax::Feature::Qi>
-
-=item L<syntax>
-
-=back
-
-=head1 AUTHOR
-
-Erik Carlsson E<lt>info@code301.comE<gt>
-
-=head1 COPYRIGHT
-
-Copyright 2014 - Erik Carlsson
-
-=head1 LICENSE
-
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
+=for :list
+* L<Syntax::Feature::Ql> (which served as a base for this)
+* L<Syntax::Feature::Qi>
+* L<String::Nudge>
+* L<syntax>
 
 =cut
